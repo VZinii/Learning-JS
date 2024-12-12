90741f459cfb65d6500ab187e48e2f43d23fe704
Vitor Pires Zini <zini.cdc@gmail.com>
2024-12-12 12:13:42 -0300

    Cap 12 pt. 1

---
+++ cap12/sandbox.js
@@ -0,0 +1,15 @@
+// A guide for web development to study later: https://developer.mozilla.org/pt-BR/docs/Learn
+
+const request = new XMLHttpRequest();
+
+request.addEventListener('readystatechange', () => {
+    if (request.readyState === 4 && request.status === 200 ){
+        console.log(request.responseText);
+    }
+    else if (request.readyState === 4) {
+        console.log('could not fetch the data');
+    }
+});
+
+request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
+request.send();
