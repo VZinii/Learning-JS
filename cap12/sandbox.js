bc4b1a5381d42ff76d904a9b5c4ad57bac9e243d
Vitor Pires Zini <zini.cdc@gmail.com>
2024-12-17 11:37:05 -0300

    Basic of Promises

--- cap12/sandbox.js
+++ cap12/sandbox.js
@@ -1,46 +1,57 @@
 // A guide for web development to study later: https://developer.mozilla.org/pt-BR/docs/Learn
 
-const getTodos = (resource, callback) => {
-    const request = new XMLHttpRequest();
+const getTodos = (resource) => {
     
-    request.addEventListener('readystatechange', () => {
-        if (request.readyState === 4 ){
-            if ( request.status === 200 ){
-                // JSON = JavaScript Object Notation. It's essencialy a string, because that's how information is passed through the internet. By text
-                // So we need to convert the string into javascript objects, so we can work better with the data
-                const data = JSON.parse(request.responseText);
-                callback(undefined, data);
+    return new Promise((resolve, reject) => {
+
+        const request = new XMLHttpRequest();
+        
+        request.addEventListener('readystatechange', () => {
+            if (request.readyState === 4 ){
+                if ( request.status === 200 ){
+                    // JSON = JavaScript Object Notation. It's essencialy a string, because that's how information is passed through the internet. By text
+                    // So we need to convert the string into javascript objects, so we can work better with the data
+                    const data = JSON.parse(request.responseText);
+                    resolve(data);
+                }
+                else {
+                    reject('could not fetch data');
+                }
             }
-            else {
-                callback('Could not fetch data', undefined);
-            }
-        }
+        });
+        
+        // request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
+        request.open('GET', resource);
+        request.send();
+
     });
-    
-    // request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
-    request.open('GET', resource);
-    request.send();
+
 };
 
-console.log(1);
-console.log(2);
+// It looks a lot better dealing with the request this way than the other way
+getTodos('todos/vitor.json').then(data => {
+    console.log(data);
+}).catch(error => {
+    console.log(error);
+});
 
-// This getTodos is an asyncrinous code, it will start after the 2, and finish after the 4. The callback function is gonna be fired after the rest of the code.
+// Promises are a way of treating data in JavaScript
 
-// This is callback hell: 
-// getTodos('todos/vitor.json', (err, data) => {
-//     console.log(data);
-//     getTodos('todos/pedro.json', (err, data) => {
-//         console.log(data);
-//         getTodos('todos/raissa.json', (err, data) => {
-//             console.log(data);
-//         });
+// Basic example of Promises:
+
+// const getSomething = () => {
+//     // The Promise has two built-in parameters we can work with (resolve and reject)
+//     return new Promise((resolve, reject) => {
+//         // fetching some data
+//         // resolve('some data');
+//         // reject('some error');
 //     });
+
+// };
+
+// getSomething().then(data => {
+//     console.log(data);
+// }).catch(error => {
+//     console.log(error);
 // });
-// To avoid them we can use promises, because sometimes we need to stay some responde to make another request
 
-// Promises is a form of treating data in JavaScript
-
-
-console.log(3);
-console.log(4);
